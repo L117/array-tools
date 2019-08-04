0828e83ad62695d8a809a27600c5f9eb50056d91
L117 <l117@l117.xyz>
2019-08-04 18:28:37 +1000

    Implemented array initialization with iterator.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Array Tools
+
+A little collection of array-related utils aiming to make life easier. Requires nightly.
+
+## Features
+- Fixed-size array (of arbitrary size) initialization with iterator.
---
+++ src/lib.rs
@@ -0,0 +1,160 @@
+#![feature(fixed_size_array)]
+use std::array::FixedSizeArray;
+use std::mem::MaybeUninit;
+use std::marker::PhantomData;
+use std::{ptr, mem};
+
+struct FixedCapacityVector<T, A: FixedSizeArray<T>> {
+    array: MaybeUninit<A>,
+    length: usize,
+    _element: PhantomData<T>
+}
+
+impl<T, A: FixedSizeArray<T>> FixedCapacityVector<T, A> {
+    fn new() -> FixedCapacityVector<T, A> {
+        FixedCapacityVector {
+            array: MaybeUninit::uninit(),
+            length: 0,
+            _element: PhantomData,
+        }
+    }
+
+    fn from_array(array: A) -> FixedCapacityVector<T, A> {
+        let length = array.as_slice().len();
+        FixedCapacityVector {
+            array: MaybeUninit::new(array),
+            length,
+            _element: PhantomData,
+        }
+    }
+
+    fn capacity(&self) -> usize {
+        unsafe {
+            (*self.array.as_ptr()).as_slice().len()
+        }
+    }
+
+    fn length(&self) -> usize {
+        self.length
+    }
+
+    fn is_empty(&self) -> bool {
+        self.length() == 0
+    }
+
+    fn is_full(&self) -> bool {
+        self.length() == self.capacity()
+    }
+
+    fn push(&mut self, element: T) {
+        if self.length() < self.capacity() {
+            let next_idx = self.length();
+            let slice = unsafe {
+                (*self.array.as_mut_ptr()).as_mut_slice()
+            };
+            unsafe {
+                ptr::write(&mut slice[next_idx], element);
+            }
+            self.length += 1;
+        } else {
+            panic!("Fixed size vector is over capacity.");
+        }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            let last_idx = self.length() - 1;
+            let item = unsafe {
+                let slice = (*self.array.as_ptr()).as_slice();
+                ptr::read(&slice[last_idx])
+            };
+            self.length -= 1;
+            Some(item)
+        }
+    }
+
+    fn try_extract_array(&mut self) -> Option<A> {
+        if self.length() == self.capacity() {
+            let array_bitwise_copy = unsafe {
+                ptr::read(self.array.as_ptr())
+            };
+            self.length = 0;
+            Some(array_bitwise_copy)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T, A: FixedSizeArray<T>> Drop for FixedCapacityVector<T, A> {
+    fn drop(&mut self) {
+        while let Some(item) = self.pop() {
+            mem::drop(item);
+        }
+    }
+}
+
+/// Attempts to initialize fixed-length array from iterator.
+/// 
+/// # Examples
+/// ```rust
+/// use array_tools;
+/// 
+/// // Attempt to initialize array with iterator yielding not enough items will result in returning
+/// // `None`. All taken items will be dropped.
+/// let maybe_array: Option<[u64; 5]> = array_tools::try_init_from_iterator(1..=4);
+/// assert_eq!(maybe_array, None);
+/// 
+/// // Attempt to initialize array with iterator yielding number of items equal to array length
+/// // will result in returning `Some` containing array.
+/// let maybe_array: Option<[u64; 5]> = array_tools::try_init_from_iterator(1..=5);
+/// assert_eq!(maybe_array, Some([1, 2, 3, 4, 5]));
+/// 
+/// // Attempt to inititalize array with iterator yielding too many items (Mare than array can 
+/// // contain) will result in taking length-of-array items from iterator and returning `Some` 
+/// // containing array. Iterator with all remaining items will be dropped.
+/// let maybe_array: Option<[u32; 5]> = array_tools::try_init_from_iterator(1..=100);
+/// assert_eq!(maybe_array, Some([1, 2, 3, 4, 5])); 
+/// ```
+pub fn try_init_from_iterator<T, A, I>(mut iter: I) -> Option<A>
+where
+    A: FixedSizeArray<T>,
+    I: Iterator<Item = T>,
+{
+    let mut vec = FixedCapacityVector::new();
+    loop {
+        if vec.is_full() {
+            break vec.try_extract_array();
+        } else {
+            if let Some(item) = iter.next() {
+                vec.push(item)
+            } else {
+                break None;
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn not_enough_items() {
+        let maybe_array: Option<[u64; 5]> = super::try_init_from_iterator(1..=4);
+        assert_eq!(maybe_array, None);
+    }
+
+    #[test]
+    fn exact_item_count() {
+        let maybe_array: Option<[u64; 5]> = super::try_init_from_iterator(1..=5);
+        assert_eq!(maybe_array, Some([1, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn too_many_items() {
+        let maybe_array: Option<[u32; 5]> = super::try_init_from_iterator(1..=100);
+        assert_eq!(maybe_array, Some([1, 2, 3, 4, 5]));
+    }
+}
